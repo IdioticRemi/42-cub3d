42afe5dd1962ad3eced77e886a903229b5c3eb51
selee <selee@student.42lyon.fr>
2022-04-15 00:29:57 +0200

    does not rotate 360

--- srcs/2d_raycaster.c
+++ srcs/2d_raycaster.c
@@ -1,15 +1,17 @@
 #include "cub3d.h"
 
-void	draw_line(t_cub *cub, int x, int start, int end, int color)
+void	draw_line(t_cub *cub, int x, int y1, int y2, int color)
 {
 	int		y;
-	y = start;
-	while (y <= end)
+	y = y1;
+	while (y <= y2)
 	{
 		mlx_pixel_put(cub->mlx, cub->win, x, y, color);
 		y++;
 	}
 }
+
+
 
 void	put_ray(t_cub *cub)
 {
@@ -18,9 +20,11 @@ void	put_ray(t_cub *cub)
 	
 	while (cub->map.array[(int)(p.pos_y / TILE_SIZE)][(int)(p.pos_x / TILE_SIZE)] != '1')
 	{
-		p.pos_x += cub->player.dir_x; 
+		// p.pos_x += cos(p.dir_x); 
+		// p.pos_y += sin(p.dir_y);
+		p.pos_x += cub->player.dir_x;
 		p.pos_y += cub->player.dir_y;
-		mlx_pixel_put(cub->mlx, cub->win, p.dir_x, p.dir_y, BLUE);
+		mlx_pixel_put(cub->mlx, cub->win, p.pos_x, p.pos_y, BLUE);
 	}
 
 }
